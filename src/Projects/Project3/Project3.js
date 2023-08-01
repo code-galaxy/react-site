d317d5f79ac25fe5041919781815bea4d4c1a68a
Yaroslav <ykarpash@gmail.com>
2023-08-01 23:18:04 +0300

    project 3

---
+++ src/Projects/Project3/Project3.js
@@ -0,0 +1,98 @@
+import React from "react";
+import "./Project3.css";
+
+
+export default function Project3() {
+  const [data, setData] = React.useState([]);
+  const [dataUser, setDataUser] = React.useState({});
+  const [visibleDataUser, setVisibleDataUser] = React.useState(false);
+  const selectRef = React.useRef();
+
+  // ==============================================================================
+
+  const getValue = () => {
+    if (selectRef.current.selectedIndex) {
+      setVisibleDataUser(true);
+      setDataUser(data[selectRef.current.selectedIndex-1]);
+    }
+  }
+  console.log(dataUser);
+
+  // ==============================================================================
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData(data);
+      });
+    console.log('componentDidMount');
+  }, []);
+
+  return (
+    <>
+      <div className="description">
+        <h1>Get user data </h1>
+      </div>
+      <div className="Project2-style Project3-style">
+        <div className="Project3-input">
+
+          <select ref={selectRef} onChange={getValue} name="select" id="Project3-select" >
+            <option key={new Date().getTime().toString()} value="" hidden>to choose customer</option>
+            {data.map((item) => <option key={item.id} value={item.id} >{item.name}</option>)}
+          </select>
+
+          <div className="Project3-checkbox">
+            <input type="checkbox" id="chk1-label" />
+            <label htmlFor="chk1-label">id</label>
+          </div>
+          <div className="Project3-checkbox">
+            <input type="checkbox" id="chk2-label" />
+            <label htmlFor="chk2-label">name</label>
+          </div>
+          <div className="Project3-checkbox">
+            <input type="checkbox" id="chk3-label" />
+            <label htmlFor="chk3-label">username</label>
+          </div>
+          <div className="Project3-checkbox">
+            <input type="checkbox" id="chk4-label" />
+            <label htmlFor="chk4-label">e-mail</label>
+          </div>
+          <div className="Project3-checkbox">
+            <input type="checkbox" id="chk5-label" />
+            <label htmlFor="chk5-label">phone</label>
+          </div>
+          <div className="Project3-checkbox">
+            <input type="checkbox" id="chk6-label" />
+            <label htmlFor="chk6-label">website</label>
+          </div>
+          <div className="Project3-checkbox">
+            <input type="checkbox" id="chk6-label" />
+            <label htmlFor="chk6-label">address</label>
+          </div>
+          <button className="Project3-btn">GET user data</button>
+        </div>
+        <div className="Project3-out">
+
+          {visibleDataUser &&
+            <ul>
+              <li>id: <span>{dataUser.id}</span></li>
+              <li>name: <span>{dataUser.name}</span></li>
+              <li>name: <span>{dataUser.username}</span></li>
+              <li>email: <span>{dataUser.email}</span></li>
+              <li>phone: <span>{dataUser.phone}</span></li>
+              <li>website: <span><a href="#" target="_blanc">www.{dataUser.website}</a></span></li>
+              <li>company: <span>{dataUser.company.name}</span></li>
+              <li>address: city <span>{dataUser.address.city}</span>, street <span>{dataUser.address.street}</span></li>
+            </ul>}
+        </div>
+      </div >
+
+      <div className="link-to-code">
+        <a href="https://css.in.ua/html/tag/a/target" target="_blanc">
+          view code
+        </a>
+      </div>
+    </>
+  );
+}
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -7,14 +7,14 @@ function Projects() {
    return (
       <div className="Sidebar-site">
          <h1>My pet (React) Projects is here</h1>
-         
+
          <section className="section project2-style">
             <div className="about-project">
                <h1>Project #3</h1>
                <h3>Get user data</h3>
                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum labore, veritatis quidem eaque nam officia et ut soluta porro blanditiis eos ducimus sint a optio necessitatibus perferendis maiores amet quos!</p>
             </div>
-            <div className="app-project">{<Project3/>}</div>
+            <div className="app-project">{<Project3 />}</div>
          </section>
 
          <section className="section project2-style">
@@ -27,7 +27,7 @@ function Projects() {
                   <li>друний варіант</li>
                </ol>
             </div>
-            <div className="app-project">{<Project2/>}</div>
+            <div className="app-project">{<Project2 />}</div>
          </section>
 
          <section className="section">
@@ -38,9 +38,6 @@ function Projects() {
             </div>
             <div className="app-project">{<Project1 />}</div>
          </section>
-
-
-
       </div>
    )
 }
