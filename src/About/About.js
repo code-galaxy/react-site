4b8c5381945ea674e8308194ea518764e70f04d2
Yaroslav <ykarpash@gmail.com>
2023-08-29 12:40:04 +0300

    edited information About

--- src/About/About.js
+++ src/About/About.js
@@ -4,6 +4,8 @@ import certificateJS from "./images/Certificate-JS.png";
 import certificateJsFunc from "./images/Certificate-JS-Function.png";
 import certificateJSArray from "./images/Certificate-JS-Array-Methods.png";
 import certificateReactJS from "./images/Certificate-ReactJS.png";
+import { Link } from 'react-router-dom';
+
 
 export default function About() {
    return (
@@ -32,35 +34,34 @@ export default function About() {
                <h2>Experience in IT</h2>
                <ul>
                   <li>Click <a href="https://github.com/code-galaxy" target="_blanc"><span>here</span></a> to see my GitHub.</li>
-                  <li>React JS</li>
-                  <li>2</li>
-                  <li>3</li>
-                  <li>4</li>
-                  <li>Development of a Landing page <br /> with use HTML, CSS, Bootstrap </li>
-                  <li>5</li>
-                  <li>Cypress test automate</li>
-                  <li>MySQL</li>
-                  <li>Git</li>
-                  <li>Bootstrap</li>
-                  <li>JIRA, Agile/Scram</li>
+                  <li><h4><Link to="/projects">Counter</Link></h4> – created with React JS, used useState().</li>
+                  <li><h4><Link to="/projects">Modal window</Link></h4> – created with React JS, used useState(), useRef().</li>
+                  <li><h4><Link to="/projects">Get user data</Link></h4>– created with React JS, used useState(), <br />useRef(), useEffect() and fetch(“ ”).</li>
+                  <li><h4><Link to="/projects">Todo list</Link></h4> - created with React JS <br />(the project is under development) </li>
+                  <li><h4><a href="https://code-galaxy.github.io/stopwatch.github.io/" target="_blanc">Stopwatch project</a></h4> – created with JavaScript, HTML and  CSS.</li>
+                  <li><h4><a href="https://github.com/code-galaxy/weather-forecast-api-github.io/tree/main" target="_blanc">Weather forecast API project </a></h4>– created with Java Script, used fetch.</li>
+                  <li><h4><a href="https://code-galaxy.github.io/slider-motivation.github.io/" target="_blanc">Slider project</a></h4> – created with JavaScript, HTML and CSS.</li>
+                  <li><h4><a href="https://code-galaxy.github.io/Tourism/">Landing page project</a></h4> – created with Bootstrap, SCSS, HTML, CSS.</li>
+                  <li><h4><a href="https://github.com/code-galaxy/QA-study/tree/main/Cypress%20-%20Automation%20Practice" target="_blanc">Cypress test automate</a></h4></li>
+                  <li><h4><a href="https://github.com/code-galaxy/MySQL" target="_blanc">MySQL</a></h4></li>
                </ul>
             </div>
             <div className="personal-qualities">
                <h2>Personal-qualities</h2>
                <ul>
-                  <li>A creative thinker, able to conceptualise innovative  <br />solution for complex problems.</li>
-                  <li>Able to prioritise tasks effectively, ensuring efficient <br /> allocation of time and resources to maximise productivity.</li>
-                  <li>Skilled in desicion making, conflict resolution and <br /> strategic planning, enabling sucsessful project execution <br /> and team efficiency.</li>
-                  <li>Highly self motivated profesional whith strong <br />intrinsic drive to excel and achieve results.</li>
-                  <li>Skilled in managing complex projects, effectively  <br />allocating resourses ande optimising team productivity.</li>
-                  <li> Able to leverege persuasive communication tachniques<br />to negotiate with key stakeholders and drive positive <br />outcomes for organizations.</li>
-                  <li>Able to mediate complex disputes, utalising diplomacy  <br />to find common ground + preserve key relationships for <br /> successful project outcomes.</li>
+                  <li>A creative thinker, able to conceptualise innovative  solution for complex problems.</li>
+                  <li>Able to prioritise tasks effectively, ensuring efficient allocation of time and resources to maximise productivity.</li>
+                  <li>Skilled in desicion making, conflict resolution and  strategic planning, enabling sucsessful project execution and team efficiency.</li>
+                  <li>Highly self motivated profesional whith strong intrinsic drive to excel and achieve results.</li>
+                  <li>Skilled in managing complex projects, effectively  allocating resourses ande optimising team productivity.</li>
+                  <li> Able to leverege persuasive communication tachniquesto negotiate with key stakeholders and drive positive outcomes for organizations.</li>
+                  <li>Able to mediate complex disputes, utalising diplomacy to find common ground + preserve key relationships for successful project outcomes.</li>
                   <li>Attentive to details, reliable, fast learner.</li>
                   <li>Independent and punctual.</li>
                </ul>
             </div>
          </div>
-         <div className='about-link-cv'><a href="https://drive.google.com/file/d/1-vN6aybL5JkXzeTudpL_JlUc6JTrrcSr/view?usp=drive_link" target='_blanc'>Download CV</a></div>
+         <div className='about-link-cv'><a href="https://drive.google.com/file/d/1-vN6aybL5JkXzeTudpL_JlUc6JTrrcSr/view?usp=drive_link" target='_blanc'>download CV</a></div>
 
          <div className='about-certificate'>
             <h2>CERTIFICATES</h2>
